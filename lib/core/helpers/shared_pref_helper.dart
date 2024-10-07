3d1136bbf2595023f56fe54d9b0fd28d9d784e47
rofidamedhat <141781059+rofidamedhat@users.noreply.github.com>
2024-10-07 23:09:31 +0300

    adding api constants like base url adding error model

--- lib/core/helpers/shared_pref_helper.dart
+++ lib/core/helpers/shared_pref_helper.dart
@@ -20,26 +20,26 @@ class SharedPrefHelper {
   }
 
   /// Saves a [value] with a [key] in the SharedPreferences.
-  static setData(String key, value) async {
-    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-    debugPrint("SharedPrefHelper : setData with key : $key and value : $value");
-    switch (value.runtimeType) {
-      case String:
-        await sharedPreferences.setString(key, value);
-        break;
-      case int:
-        await sharedPreferences.setInt(key, value);
-        break;
-      case bool:
-        await sharedPreferences.setBool(key, value);
-        break;
-      case double:
-        await sharedPreferences.setDouble(key, value);
-        break;
-      default:
-        return null;
-    }
-  }
+  // static setData(String key, value) async {
+  //   SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+  //   debugPrint("SharedPrefHelper : setData with key : $key and value : $value");
+  //   switch (value.runtimeType) {
+  //     case String:
+  //       await sharedPreferences.setString(key, value);
+  //       break;
+  //     case int:
+  //       await sharedPreferences.setInt(key, value);
+  //       break;
+  //     case bool:
+  //       await sharedPreferences.setBool(key, value);
+  //       break;
+  //     case double:
+  //       await sharedPreferences.setDouble(key, value);
+  //       break;
+  //     default:
+  //       return null;
+  //   }
+  // }
 
   /// Gets a bool value from SharedPreferences with given [key].
   static getBool(String key) async {
